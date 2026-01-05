95d017bd8d224db8473f78e3c14f87bda1e97906
shox3434 <shoxjaxon3434@.gmail.com>
2026-01-05 10:30:13 +0300

    yana2

---
+++ cars/cars.js
@@ -0,0 +1,167 @@
+const cars = [
+    {
+        id: 1,
+        make: 'Toyota',
+        model: 'Camry',
+        year: 2020,
+        title: 'Reliable Sedan',
+        img: 'https://www.pngmart.com/files/22/Toyota-Camry-PNG-Photo.png',
+        
+   
+    },
+
+    {
+    id: 2,
+    make: 'Honda',
+    model: 'Civic',
+    year: 2019,
+    title: 'Compact and Efficient',
+    img: 'https://www.pngmart.com/files/4/Honda-Civic-PNG-Photo.png'
+    },
+
+    {
+    id: 3,
+    make: 'Ford',
+    model: 'Mustang',
+    year: 2021,
+    title: 'Sporty Performance',
+    img: 'https://i.pinimg.com/originals/a1/0e/19/a10e19c0354de63803091dedd6dba0ca.png'
+    },
+
+    {
+    id: 4,
+    make: 'Chevrolet',
+    model: 'Impala',
+    year: 2018,
+    title: 'Spacious and Comfortable',
+    img: 'https://tse1.mm.bing.net/th/id/OIP.Lw-VjNslNE2HwpsM4X6rtAHaE0?rs=1&pid=ImgDetMain&o=7&rm=3'
+    },
+
+    {
+    id: 5,
+    make: 'BMW',
+    model: '3 Series',
+    year: 2022,
+    title: 'Luxury and Style',
+    img: 'https://tse1.mm.bing.net/th/id/OIP.h9tSMKFi8Acq--o3GuU_UQHaDK?rs=1&pid=ImgDetMain&o=7&rm=3'
+    },
+
+    {
+    id: 6,
+    make: 'Audi',
+    model: 'A4',
+    year: 2021,
+    title: 'Sophisticated Design',
+    img: 'https://vexstockimages.fastly.carvana.io/stockimages/2021_AUDI_A4_45%20TFSI%20PREMIUM%20PLUS%20SEDAN%204D_WHITE_stock_desktop_1920x1080.png?v=1645599681.760&quality=100&width=1080'
+    },
+
+    {
+    id: 7,
+    make: 'Mercedes',
+    model: 'C-Class',
+    year: 2020,
+    title: 'Premium Experience',
+    img: 'https://tse3.mm.bing.net/th/id/OIP.ljx4ZVob-TkwXSIcaMAbqQHaD9?rs=1&pid=ImgDetMain&o=7&rm=3'
+    },
+
+    {
+    id: 8,
+    make: 'Volkswagen',
+    model: 'Golf',
+    year: 2019,
+    title: 'Versatile Hatchback',
+    img: 'https://th.bing.com/th/id/R.cb6f0667abd9b78e164922f2744222a9?rik=mh6U7BgHHeNjbg&pid=ImgRaw&r=0'
+    },
+
+    {
+    id: 9,
+    make: 'Subaru',
+    model: 'Outback',
+    year: 2021,
+    title: 'Adventure Ready',
+    img: 'https://platform.cstatic-images.com/xxlarge/in/v2/stock_photos/d10fefa1-f240-4992-873b-99d73b43d7bb/971cf22c-149f-4822-a53e-2b0891b91e59.png'
+    },
+
+    {
+    id: 10,
+    make: 'Tesla',
+    model: 'Model 3',
+    year: 2022,
+    title: 'Electric Innovation',
+    img: 'https://www.pngmart.com/files/22/Tesla-Model-3-PNG-Isolated-Photo.png'
+    }
+]
+
+
+let div = document.getElementById("containar");
+
+let select = document.getElementById("selectYear");
+
+
+function getData(sortedCars) {
+
+div.innerHTML = "";
+let carsToDisplay;
+
+if (sortedCars && sortedCars.length > 0) {
+    carsToDisplay = sortedCars;
+} else {
+    carsToDisplay = cars;
+}
+
+carsToDisplay.forEach(item => {
+
+    let newDiv = document.createElement("div");
+
+    newDiv.innerHTML = `
+        <div class="cars">
+        <img src="${item.img}" alt="">
+        <h1>${item.make}</h1>
+        <h2>${item.model}</h2>
+        <h3>${item.year}</h3>
+        <p>${item.title}</p>
+    </div>`
+
+    div.appendChild(newDiv);
+})
+} getData();
+
+
+
+select.addEventListener("change", () => {
+   if (select.value == "new") {
+    let sortedCars = cars.sort((a, b) => {  
+    return b.year - a.year;
+
+    
+   });
+
+   getData(sortedCars); 
+   }
+   else if (select.value == "old") {
+    let sortedCars = cars.sort((a, b) => {  
+        return a.year - b.year;
+       })
+   getData(sortedCars);
+   
+}
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nom.js
@@ -0,0 +1,111 @@
+// harflarni kichkina qiberadu
+
+// let name = "Shakh";
+
+// // console.log(name.toLowerCase());
+
+
+// hariflarni katta qiberadu
+
+// let name = "Shakh";
+
+// console.log(name.toUpperCase());
+
+// ozgaruvchini uzunligini aniqlaydi
+
+// let name = "Shakh";
+
+// console.log(name.length);
+
+// boshidagi va oxiridagi bo'sh joylarni olib tashlaydi
+
+// let name = " Shakh";
+
+// console.log(name.trim());
+
+// ozgaruvchida ma'lum bir harf yoki so'z bor-yo'qligini tekshiradi
+
+// let name = " Shakh";
+
+// console.log(name.includes(""));
+
+
+// bu holatda kichkina harf bilan tekshiradi
+
+// let name = " Shakh";
+
+// console.log(name.toLowerCase().includes("s"));
+
+
+
+// berilgan indeks oralig'idagi harflarni kesib oladi
+
+// let name = "Shakh";
+
+// console.log(name.slice(0,4));
+
+
+
+// berilgan harfni yoki so'zni boshqasi bilan almashtiradi
+
+
+// let name = "Shakh";
+
+// console.log(name.replace("Shakh", "Dav"));
+
+
+
+
+
+// ozgaruvchini bo'sh joylar bo'yicha ajratib, massivga aylantiradi
+
+
+// let name = "Shakh";
+
+// console.log(name.split(" "));
+
+
+
+// massivga yangi element qo'shadi
+
+// let name = ["Shakh", "Dav", "Ali"]
+// name.push("yosh 23")
+// console.log(name);
+
+
+
+
+// hammasini birbiriga qo'shadi
+
+// let sonlar = [1, 2, 3, 4, 5]
+
+// let qiymat = sonlar.reduce((Index, item)=> Index + item, 0)
+
+// console.log(qiymat);
+
+
+
+
+// massivni teskari tartibda chiqaradi agar ornini almashtrsa togri chiqaradi
+
+// let sonlar = [1, 2, 3, 4, 5]
+
+// let qiymat = sonlar.sort((Index, item)=> item - Index)
+
+// console.log(qiymat);
+
+
+
+
+
+// berilgan shartga mos keladigan elementlarni chiqaradi
+
+// let sonlar = ["olma", "anor", "behi", "shaftoli", "o'rik"]
+
+// let qiymat = sonlar.filter(fruit=>{
+//     return fruit.includes("o")
+// })
+
+// console.log(qiymat);
+
+
