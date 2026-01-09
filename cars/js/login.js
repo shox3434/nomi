dadf3e61f7dae667488a56003b6ef7bfadda2eef
shox3434 <shoxjaxon3434@.gmail.com>
2026-01-09 19:41:04 +0300

    l

--- cars/js/login.js
+++ cars/js/login.js
@@ -4,24 +4,44 @@ let btn = document.getElementById("send")
 
 btn.addEventListener("click",(e)=>{
  e.preventDefault()
-    let ism  = "Muhammad"
-    let pass  = "Muhammad2002"
-let localname = name.value
-let localpass = parol.value
+ let user = {
+        username: name.value,
+        password: parol.value
+}
 
-    
-    if(localname.toLowerCase() == ism.toLowerCase() && localpass.trim() == pass){
-        alert("siz Ro'yxatdan o'tdingiz")
-        localStorage.setItem("token",JSON.stringify({localname,localpass}))
-        window.location.href = "./index.html"
-    }
-    else if(localpass == "" && localname == ""||localpass.length<8){
-        alert("siz Nimadur Yozishingiz kerak yoki parol kam kiritingiz")
-    }
-    else{
-        alert("qandaydur xatolik bor")
-    }
-   localname = ""
-    localpass = ""
+
+
+fetch('https://dummyjson.com/auth/login',{
+
+method:"POST",
+headers:{
+    "Content-Type":"application/json"
+},
+body:JSON.stringify(user)
+
+})
+.then(res=>res.json())
+.then(data=>{
+   if(data.accessToken){ 
+    localStorage.setItem("token",JSON.stringify(data.accessToken))
+    window.location.href = "../cars/index.html"
+   }
+   else{
+    alert = "royxatdan otishingiz kerak"
+   }
+})
+//     if(localname.toLowerCase() == ism.toLowerCase() && localpass.trim() == pass){
+//         alert("siz Ro'yxatdan o'tdingiz")
+//         localStorage.setItem("token",JSON.stringify({localname,localpass}))
+//         window.location.href = "./index.html"
+//     }
+//     else if(localpass == "" && localname == ""||localpass.length<8){
+//         alert("siz Nimadur Yozishingiz kerak yoki parol kam kiritingiz")
+//     }
+//     else{
+//         alert("qandaydur xatolik bor")
+//     }
+//    localname = ""
+//     localpass = ""
 
 })
--- cars/js/main.js
+++ cars/js/main.js
@@ -1,36 +1,75 @@
+let saved = JSON.parse(localStorage.getItem("token"))
 
-fetch('https://dummyjson.com/products')
+if(saved){
+   fetch('https://dummyjson.com/products')
 .then(res => res.json())
 .then(data => getdata(data.products))
+}
+
+else{
+   alert= "royhatdan oting"
+   window.location.href = "../cars/login.html"
+}
+
+
+
 
 let $div = document.getElementById("containar")
+
+let searchInp = document.querySelector(".inp")
+
+const truncate = (text, limit) =>{
+   if(text.length > 20){
+      return text.slice(0,limit) + "..." 
+    }
+
+    else{
+      return text
+    }
+}
+
 function getdata(item){
- for (let i = 0; i < item.length; i++){
-    
-    let mahsulot = item[i]
+
+   $div.innerHTML = ""
+
+item.map(mahsulot => {
+
+ 
 
     let newLi = document.createElement("li")
 
     newLi.innerHTML = `
-    
+   
    <div class="card">
-    
-    <a href="./single.html?card-id=${mahsulot.id}"> 
+     <a href="./single.html?card-id=${(mahsulot.id)}">
+     
         <img src="${mahsulot.thumbnail}" alt="">
-     </a>
+    
 
     <div class="card_body">
     <h1 class="card_title">${mahsulot.title}</h1>
-    <p class="card_description">${mahsulot.description}</p>
+    <p class="card_description">${truncate(mahsulot.description,50)}</p>
+    <p class="card_rating">${"⭐".repeat(Math.round(mahsulot.rating))}</p>
     <h5 class="card_price">${mahsulot.price}</h5>
     </div>
+    </a>
     </div>
 
+     
     
     `
-    $div.appendChild(newLi) 
- }
+      $div.appendChild(newLi)
+ 
+})
 
+ 
 }
-    
 
+searchInp.addEventListener("input", (e) => {
+   console.log(e.target.value);
+fetch(`https://dummyjson.com/products/search?q=${e.target.value.toLowerCase()}`)
+.then(res => res.json())
+.then(data =>{
+      getdata(data.products)
+});
+})
